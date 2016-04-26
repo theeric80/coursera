2a6428f307fbfbb4c4e0721b07d1c80a3cba468c
Eric Tsai <chunchieh@gmail.com>
2016-04-26 10:35:05 +0000

    [what] add assignment 5: part 2

--- recommender-systems/06_item-based/movie_rating_dao.py
+++ recommender-systems/06_item-based/movie_rating_dao.py
@@ -6,27 +6,36 @@ def mean(data):
 class MovieRatingDao(AbstractDaoCSV):
     def __init__(self, filename):
         AbstractDaoCSV.__init__(self, filename)
+        self._norm_ratings = []
 
     def _fmt_value(self, x):
         return float(x) if x else 0.0
 
-    def normalize(self):
+    def _build_norm_ratings(self):
         result = []
         n = self.total_records()
-        for i in xrange(n):
-            record = self.get_record(i)
-            mu = mean([x for x in record if x])
+        for m in xrange(n):
+            record = self.get_record(m)
+            mu = mean(filter(bool, record))
             result.append(map(lambda x: x-mu if x else x, record))
-        self._records = result
+        self._norm_ratings = result
 
     def movie_id(self, i):
-        return self.get_record_name(i).split(':')[0]
+        return self.get_header(i).split(':')[0]
 
     def total_movies(self):
         return self.total_fields()
 
     def get_movie_ratings(self, i):
         return self.get_field(i)
+
+    def get_norm_movie_ratings(self, i):
+        if not self._norm_ratings:
+            self._build_norm_ratings()
+        return [record[i] for record in self._norm_ratings]
+
+    def get_user_ratings(self, i):
+        return self.get_record(i)
 
 if __name__ == '__main__':
     def main():
@@ -35,11 +44,6 @@ if __name__ == '__main__':
         n = dao.total_records()
 
         print '===== Records ====='
-        for i in xrange(n):
-            print dao.get_record(i)
-
-        print '===== Normalized Records ====='
-        dao.normalize()
         for i in xrange(n):
             print dao.get_record(i)
 
--- recommender-systems/06_item-based/recommend_service.py
+++ recommender-systems/06_item-based/recommend_service.py
@@ -2,20 +2,29 @@ import math
 import operator
 
 class RecommendService(object):
-    def __init__(self, item_rating_dao, item_correlation_dao):
+    def __init__(self, item_rating_dao, item_correlation_dao, normalized=False):
         object.__init__(self)
 
         self._item_rating_dao = item_rating_dao
         self._item_correlation_dao = item_correlation_dao
 
-        self._build_item_correlation()
+        self._build_item_correlation(normalized)
 
     @property
     def total_items(self):
         return self._item_rating_dao.total_movies()
 
+    def _user_ratings(self, i):
+        return self._item_rating_dao.get_user_ratings(i)
+
     def _item_ratings(self, i):
         return self._item_rating_dao.get_movie_ratings(i)
+
+    def _norm_item_ratings(self, i):
+        return self._item_rating_dao.get_norm_movie_ratings(i)
+
+    def _item_correl(self, i, j):
+        return self._item_correlation_dao.get_correlation(i, j)
 
     def _item_correls(self, i):
         return self._item_correlation_dao.get_correlations(i)
@@ -29,12 +38,13 @@ class RecommendService(object):
     def _cosine_sim(self, v1, v2):
         return self._dot_product(v1, v2) / (self._magnitude(v1) * self._magnitude(v2))
 
-    def _build_item_correlation(self):
+    def _build_item_correlation(self, normalized):
+        _item_ratings = self._norm_item_ratings if normalized else self._item_ratings
         m = self.total_items
         for m1 in xrange(m):
-            r1 = self._item_ratings(m1)
+            r1 = _item_ratings(m1)
             for m2 in xrange(m):
-                r2 = self._item_ratings(m2)
+                r2 = _item_ratings(m2)
                 sim = self._cosine_sim(r1, r2)
                 self._item_correlation_dao.set_correlation(m1, m2, sim)
         """
@@ -50,6 +60,20 @@ class RecommendService(object):
 
     def top_n_similiar_items(self, i, n):
         return self._get_sorted_similiar_items(i)[:n]
+
+    def predict(self, uid, m):
+        ratings = [(i, r) for i, r in enumerate(self._user_ratings(uid)) if r]
+        ratings = [(r, self._item_correl(i, m)) for i, r in ratings]
+        ratings = filter(lambda (r, w): w>0, ratings)
+        return sum(r*w  for r, w  in ratings) / sum(w for _, w in ratings)
+
+    def predict_all(self, uid):
+        return [(m, self.predict(uid, m)) for m in xrange(self.total_items)]
+
+    def top_n_recommended_items(self, uid, n):
+        items = self.predict_all(uid)
+        items.sort(key=operator.itemgetter(1), reverse=True)
+        return items[:n]
 
 if __name__ == '__main__':
     def main():
@@ -68,19 +92,29 @@ if __name__ == '__main__':
         print '===== Top 5 Toy Story ====='
         toy_story = 0
         for i, x in recommender.top_n_similiar_items(toy_story, 5):
-            print '{:4s}: {}'.format(movie_rating_dao.movie_id(i), x)
+            print '({:2d}){:4s}: {}'.format(i, movie_rating_dao.movie_id(i), x)
+
+        print '===== Top 5 for User 5277 ====='
+        user = 1
+        for i, x in recommender.top_n_recommended_items(user, 5):
+            print '({:2d}){:4s}: {}'.format(i, movie_rating_dao.movie_id(i), x)
 
         print '\n\nNormalize Ratings'
-        movie_rating_dao.normalize()
         item_correlation_dao = ItemCorrelationDao(m)
 
         recommender = RecommendService(
             movie_rating_dao,
-            item_correlation_dao)
+            item_correlation_dao,
+            normalized=True)
 
         print '===== Top 5 Toy Story ====='
         toy_story = 0
         for i, x in recommender.top_n_similiar_items(toy_story, 5):
-            print '{:4s}: {}'.format(movie_rating_dao.movie_id(i), x)
+            print '({:2d}){:4s}: {}'.format(i, movie_rating_dao.movie_id(i), x)
+
+        print '===== Top 5 for User 5277 ====='
+        user = 1
+        for i, x in recommender.top_n_recommended_items(user, 5):
+            print '({:2d}){:4s}: {}'.format(i, movie_rating_dao.movie_id(i), x)
 
     main()
